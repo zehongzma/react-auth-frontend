f8611a3a15e76e8df2dd8277b0b44325dd89f8ca
Zehong Z Ma <zehong z ma@ZMA012SPE2.local>
2018-11-15 14:31:00 +0800

    changed API_URL variable

--- src/config.js
+++ src/config.js
@@ -1,3 +1,1 @@
-export const API_URL = process.env.NODE_ENV === 'production' 
-  ? 'https://react-auth-twitter.herokuapp.com'
-  : 'https://hkappdlv006:80'
+export const API_URL = 'https://hkappdlv006:80'
